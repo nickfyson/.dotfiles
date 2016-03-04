7640cdd1f47d0ba960ac01ea6088c069d75d32a2
Nick Fyson <nick@fyson.net>
2016-03-04 16:17:42 +0000

    Update to fully functioning version
    Includes sizeable amount of customisation for primary computer hidden in (untracked) folder 'custom'.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 
 # symbolically link the .bashrc file
-ln -sfv $HOME/.dotfiles/shell/bashrc $HOME/.bashrc
+echo 'if [ -f ~/.dotfiles/master_bashrc.sh ]; then source ~/.dotfiles/master_bashrc.sh; fi' > $HOME/.bashrc
 # for cross-platform compatibility, we also create a .bash_profile that
 # sources from .bashrc
 echo 'if [ -f ~/.bashrc ]; then source ~/.bashrc; fi' > $HOME/.bash_profile
+
+source $HOME/.bashrc
---
+++ master_bashrc.sh
@@ -0,0 +1,22 @@
+
+# define custom prompt
+export PS1="\[\e[34;1m\]\h \[\e[0;31m\]\W\[\e[m\] ☕  "
+
+
+# set default editor
+export EDITOR='vim'
+
+# customize the use of colours on the terminal
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+
+# customise a couple of standard command line utlities
+alias lf='ls -lg'
+alias rm='rm -i'
+
+
+for BASHRCFILE in `find .dotfiles -name "bashrc"`
+do
+  [ -f "$BASHRCFILE" ] && source "$BASHRCFILE"
+done
