952188590ed028845d4122daa46dbcccec37aabe
Nick Fyson <nick@fyson.net>
2018-10-24 22:09:23 +0100

    Only run OS X installation on Darwin systems

--- osx/install.sh
+++ osx/install.sh
@@ -1,3 +1,7 @@
-source $HOME/.dotfiles/osx/defaults.sh
-source $HOME/.dotfiles/osx/brew.sh
-
+if [ "$(uname)" == "Darwin" ]; then
+    # TODO code to install homebrew essentials
+    # and X-Code tools as required
+    :
+    source $HOME/.dotfiles/osx/defaults.sh
+	source $HOME/.dotfiles/osx/brew.sh
+fi
