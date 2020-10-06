13edeb4c0fe5ab8dbbca1a7ab1c013d022f2a461
Nick Fyson <nickfyson@github.com>
2020-10-06 22:47:43 +0100

    reboot dotfiles and switch to zsh

--- linux/install.sh
+++
@@ -1,5 +0,0 @@
-
-if [ "$(uname)" == "Linux" ]; then
-    eval $pkgman update
-    eval $pkgman install -y build-essential python-dev git CMake ruby-dev
-fi
--- osx/install.sh
+++
@@ -1,7 +0,0 @@
-if [ "$(uname)" == "Darwin" ]; then
-    # TODO code to install homebrew essentials
-    # and X-Code tools as required
-    :
-    source $HOME/.dotfiles/osx/defaults.sh
-	source $HOME/.dotfiles/osx/brew.sh
-fi
